2ac0b6899502ca8c162ad9822cc24f09495a91da
nik gaffney <nik@fo.am>
2024-01-14 16:29:01 +0100

    Put in earplugs

--- packages/zzkt/soupault.scm
+++ packages/zzkt/soupault.scm
@@ -24,6 +24,7 @@
   #:use-module (guix build-system dune)
   #:use-module (guix build-system ocaml)
   #:use-module (gnu packages ocaml)
+  #:use-module (gnu packages ocaml-base)
   #:use-module (gnu packages glib)
   #:use-module (gnu packages libevent)
   #:use-module (gnu packages pkg-config)
