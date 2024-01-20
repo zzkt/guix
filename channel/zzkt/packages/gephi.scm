4bc0ceb67a0cce70963e81347fd6731dd246ce11
nik gaffney <nik@fo.am>
2024-01-20 10:21:59 +0100

    Put in earplugs

--- channel/zzkt/packages/gephi.scm
+++ channel/zzkt/packages/gephi.scm
@@ -16,7 +16,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-define-module (zzkt packages gephi)
+(define-module (zzkt packages gephi)
   #:use-module (guix)
   #:use-module (guix build-system ant)
   #:use-module (guix git-download)
