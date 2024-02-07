e45f9c850f27333ac4cd72bfdfe030cfc9d53c64
nik gaffney <nik@fo.am>
2024-02-07 18:42:27 +0100

    Put in earplugs

--- config/home-configuration.scm
+++ config/home-configuration.scm
@@ -1,13 +1,21 @@
 ;; -*- mode: scheme;  coding: utf-8; -*-
 ;;
-;; tangled from home-configuration.org on 2024-02-07 18:22:13+01:00)
+;; tangled from home-configuration.org on 2024-02-07 18:41:00+01:00)
 
 (use-modules (gnu)
              (gnu home)
              (gnu packages)
+             (gnu services)
              (gnu packages gnupg)
+             (gnu packages shells)
+             (guix channels)
              (guix gexp)
+             (gnu home services)
+             (gnu home services guix)
              (gnu home services shells)
+             (gnu home services dotfiles)
+             (gnu home services desktop)
+             (gnu home services sound)
              (gnu home services gnupg))
 
 (home-environment
@@ -49,71 +57,67 @@
              )))
 
  (services
-   (list
+  (list
 
-    (simple-service 'variant-packages-service
-                    home-channels-service-type
-                    (list
-                     (channel
-                      (name 'nonguix)
-                      (url "https://gitlab.com/nonguix/nonguix")
-                      (branch "master")
-                      (commit
-                       "fe2fcf125cfc5f7284b24cfac50f37feaf74f7b5")
-                      (introduction
-                       (make-channel-introduction
-                        "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-                        (openpgp-fingerprint
-                         "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-                     (channel
-                      (name 'zzkt)
-                      (url "https://gitlab.com/zzkt/guix")
-                      (branch "endless")
-                      (commit
-                       "ef1868ed9bdcf1a49771442e405bd88207b3ab0c")
-                      (introduction
-                       (make-channel-introduction
-                        "fc0ada85de1980e1fc9ee50672d827c0c17c3e7d"
-                        (openpgp-fingerprint
-                         "24A7 4604 91E6 A60F 5BB4  A00F 989F 5E6E DB47 8160"))))))
+   (simple-service 'variant-packages-service
+                   home-channels-service-type
+                   (list
+                    (channel
+                     (name 'nonguix)
+                     (url "https://gitlab.com/nonguix/nonguix")
+                     (branch "master")
+                     (commit
+                      "fe2fcf125cfc5f7284b24cfac50f37feaf74f7b5")
+                     (introduction
+                      (make-channel-introduction
+                       "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+                       (openpgp-fingerprint
+                        "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+                    (channel
+                     (name 'zzkt)
+                     (url "https://gitlab.com/zzkt/guix")
+                     (branch "endless")
+                     (commit
+                      "ef1868ed9bdcf1a49771442e405bd88207b3ab0c")
+                     (introduction
+                      (make-channel-introduction
+                       "fc0ada85de1980e1fc9ee50672d827c0c17c3e7d"
+                       (openpgp-fingerprint
+                        "24A7 4604 91E6 A60F 5BB4  A00F 989F 5E6E DB47 8160"))))))
 
-    (simple-service 'some-useful-env-vars-service
-                     home-environment-variables-service-type
-                     `(("NOTMUCH_PROFILE" . "$HOME/.config/notmuch/notmuch.conf")
-                       ("LD_LIBRARY_PATH" . "$HOME/.guix-profile/lib")
-                       ("SHELL" . ,(file-append zsh "/bin/zsh"))))
+   (simple-service 'variant-env-vars-service
+                   home-environment-variables-service-type
+                   `(("NOTMUCH_PROFILE" . "$HOME/.config/notmuch/notmuch.conf")
+                     ("LD_LIBRARY_PATH" . "$HOME/.guix-profile/lib")
+                     ("SHELL" . ,(file-append zsh "/bin/zsh"))))
 
-    (service home-zsh-service-type
-                  (home-zsh-configuration
-                   ;; configs in $XDG_CONFIG_HOME/zsh
-                   (xdg-flavor? #t)
-                   (environment-variables
-                    '(("HISTFILE" . "$HOME/.config/zsh/.history")
-                      ("HISTSIZE" . "800000")
-                      ("SAVEHIST" . "800000")))
-                   (zshenv
-                    (list (local-file ".zshenv" "zshenv")))
-                   (zshrc
-                    (list (local-file ".zshrc" "zshrc")))))
+   (service home-zsh-service-type
+            (home-zsh-configuration
+             ;; configs in $XDG_CONFIG_HOME/zsh
+             (xdg-flavor? #t)
+             (environment-variables
+              '(("HISTFILE" . "$HOME/.config/zsh/.history")
+                ("HISTSIZE" . "800000")
+                ("SAVEHIST" . "800000")))
+             (zshenv
+              (list (local-file ".zshenv" "zshenv")))
+             (zshrc
+              (list (local-file ".zshrc" "zshrc")))))
 
-         (service home-gpg-agent-service-type
-                  (home-gpg-agent-configuration
-                   (pinentry-program
-                    (file-append pinentry-emacs "/bin/pinentry-emacs"))
-                   (ssh-support? #t)))
+   (service home-gpg-agent-service-type
+            (home-gpg-agent-configuration
+             (pinentry-program
+              (file-append pinentry-emacs "/bin/pinentry-emacs"))
+             (ssh-support? #t)))
 
-         (service home-dotfiles-service-type
-                  (home-dotfiles-configuration
-                   (directories (list "dotfiles"))))
+   (service home-dotfiles-service-type
+            (home-dotfiles-configuration
+             (directories (list "dotfiles"))))
 
+   (service home-dbus-service-type)
 
+   (service home-pipewire-service-type
+            (home-pipewire-configuration
+             (enable-pulseaudio? #t)))
 
-        (service home-pipewire-service-type
-                 (home-pipewire-configuration
-                 (pipewire "pipewire")
-                 (wireplumber "wireplumber")
-                 (enable-pulseaudio? #t)))
-
-         ;; see -> https://guix.gnu.org/manual/devel/en/html_node/Fonts-Home-Services.html
-
-  ))) ;; end home-environment
+   ))) ;; end home-environment
