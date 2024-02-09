854ee0310a99c7114211cc89cd8e5c133b8966f9
nik gaffney <nik@fo.am>
2024-02-09 15:45:31 +0100

    Put in earplugs

--- channel/zzkt/packages/kitty.scm
+++ channel/zzkt/packages/kitty.scm
@@ -163,7 +163,7 @@
            pkg-config
            python-sphinx
            wayland-protocols
-           go@1.21.5))
+           go-1.20))
     (inputs
      (list fontconfig
            freetype
