c71714725cd9ad865d452e137962ec90c107cea3
nik gaffney <nik@fo.am>
2024-03-06 14:21:18 +0100

    Put in earplugs

--- channel/zzkt/packages/zotero.scm
+++ channel/zzkt/packages/zotero.scm
@@ -27,69 +27,69 @@
 ;; or via flatpack? https://flathub.org/apps/org.zotero.Zotero
 
 
-(define-module (zzkt packages zotero)
-  #:use-module (guix packages)
-  #:use-module (gnu packages)
-  #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix download)
-  #:use-module (gnu packages base)
-  #:use-module (gnu packages gcc)
-  #:use-module (gnu packages gtk)
-  #:use-module (gnu packages xorg)
-  #:use-module (gnu packages glib)
-  ;; #:use-module (nonguix build-system binary) ;; requires guix/guile path config?
-  #:use-module (ice-9 regex))
+;; (define-module (zzkt packages zotero)
+;;   #:use-module (guix packages)
+;;   #:use-module (gnu packages)
+;;   #:use-module ((guix licenses) #:prefix license:)
+;;   #:use-module (guix download)
+;;   #:use-module (gnu packages base)
+;;   #:use-module (gnu packages gcc)
+;;   #:use-module (gnu packages gtk)
+;;   #:use-module (gnu packages xorg)
+;;   #:use-module (gnu packages glib)
+;;   #:use-module (nonguix build-system binary) ;; requires guix/guile path config?
+;;   #:use-module (ice-9 regex))
 
-(define-public zotero
-  (package
-    (name "zotero")
-    (version "6.0.26")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (string-append "https://download.zotero.org/client/release/"
-                           version "/Zotero-" version "_linux-x86_64.tar.bz2"))
-       (sha256
-        (base32 "0h1bizcqlk22h7nvhnyp3ppymv2hrk7133hgmp15hl3bvfzz7nh6"))))
-    (build-system binary-build-system)
-    (arguments
-     `(#:patchelf-plan `(("zotero-bin" ("gcc:lib" "glibc"))
-                         ("libmozgtk.so" ("gtk+"))
-                         ("libxul.so" ("libx11" "dbus-glib" "libxt"
-                                       "libgthread")))
-       #:install-plan '(("./" "/opt/zotero/"))
-       #:phases (modify-phases %standard-phases
-                  (add-after 'patch-usr-bin-file 'patch-launcher
-                    (lambda* (#:key outputs #:allow-other-keys)
-                      (substitute* "zotero"
-                        ((,(regexp-quote
-                                         "$(dirname \"$(readlink -f \"$0\")\")"))
-                         (string-append (assoc-ref outputs "out")
-                                        "/opt/zotero")))))
-                  (add-after 'install 'install-bin
-                    (lambda* (#:key outputs #:allow-other-keys)
-                      (mkdir-p (string-append (assoc-ref outputs "out") "/bin"))
-                      (symlink (string-append (assoc-ref outputs "out")
-                                              "/opt/zotero/zotero")
-                               (string-append (assoc-ref outputs "out")
-                                              "/bin/zotero"))
-                      (mkdir-p (string-append (assoc-ref outputs "out")
-                                              "/share/applications"))
-                      (symlink (string-append (assoc-ref outputs "out")
-                                              "/opt/zotero/zotero.desktop")
-                               (string-append (assoc-ref outputs "out")
-                                "/share/applications/zotero.desktop")))))))
+;; (define-public zotero
+;;   (package
+;;     (name "zotero")
+;;     (version "6.0.26")
+;;     (source
+;;      (origin
+;;        (method url-fetch)
+;;        (uri (string-append "https://download.zotero.org/client/release/"
+;;                            version "/Zotero-" version "_linux-x86_64.tar.bz2"))
+;;        (sha256
+;;         (base32 "0h1bizcqlk22h7nvhnyp3ppymv2hrk7133hgmp15hl3bvfzz7nh6"))))
+;;     (build-system binary-build-system)
+;;     (arguments
+;;      `(#:patchelf-plan `(("zotero-bin" ("gcc:lib" "glibc"))
+;;                          ("libmozgtk.so" ("gtk+"))
+;;                          ("libxul.so" ("libx11" "dbus-glib" "libxt"
+;;                                        "libgthread")))
+;;        #:install-plan '(("./" "/opt/zotero/"))
+;;        #:phases (modify-phases %standard-phases
+;;                   (add-after 'patch-usr-bin-file 'patch-launcher
+;;                     (lambda* (#:key outputs #:allow-other-keys)
+;;                       (substitute* "zotero"
+;;                         ((,(regexp-quote
+;;                                          "$(dirname \"$(readlink -f \"$0\")\")"))
+;;                          (string-append (assoc-ref outputs "out")
+;;                                         "/opt/zotero")))))
+;;                   (add-after 'install 'install-bin
+;;                     (lambda* (#:key outputs #:allow-other-keys)
+;;                       (mkdir-p (string-append (assoc-ref outputs "out") "/bin"))
+;;                       (symlink (string-append (assoc-ref outputs "out")
+;;                                               "/opt/zotero/zotero")
+;;                                (string-append (assoc-ref outputs "out")
+;;                                               "/bin/zotero"))
+;;                       (mkdir-p (string-append (assoc-ref outputs "out")
+;;                                               "/share/applications"))
+;;                       (symlink (string-append (assoc-ref outputs "out")
+;;                                               "/opt/zotero/zotero.desktop")
+;;                                (string-append (assoc-ref outputs "out")
+;;                                 "/share/applications/zotero.desktop")))))))
 
-(inputs (list coreutils
-                gcc
-                gtk+
-                libx11
-                 glib
-                libxt
-                dbus-glib))
-   (synopsis
-    "Your personal research assistant.")
-   (description
-    "Zotero is a free, easy-to-use tool to help you collect, organize, annotate, cite, and share research.")
-   (home-page "https://www.zotero.org/")
-   (license license:gpl3)))
+;; (inputs (list coreutils
+;;                 gcc
+;;                 gtk+
+;;                 libx11
+;;                  glib
+;;                 libxt
+;;                 dbus-glib))
+;;    (synopsis
+;;     "Your personal research assistant.")
+;;    (description
+;;     "Zotero is a free, easy-to-use tool to help you collect, organize, annotate, cite, and share research.")
+;;    (home-page "https://www.zotero.org/")
+;;    (license license:gpl3)))
