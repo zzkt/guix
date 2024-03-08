3fd22e973eaafd7812078d17ace71a01915e5fce
nik gaffney <nik@fo.am>
2024-03-08 12:13:47 +0100

    Put in earplugs

--- channel/zzkt/packages/sbcl-osc.scm
+++ channel/zzkt/packages/sbcl-osc.scm
@@ -28,24 +28,24 @@
   (let ((commit "f647738ccc22925ed740a8ca9132fda76a05baeb")
         (revision "0"))
     (package
-      (name "sbcl-osc")
-      (version (git-version "0.9" revision commit))
-      (source
+     (name "sbcl-osc")
+     (version (git-version "0.9" revision commit))
+     (source
        (origin
          (method git-fetch)
          (uri (git-reference
                (url "https://github.com/zzkt/osc")
                (commit commit)))
-         (file-name (git-file-name "osc" version))
+         (file-name (git-file-name "cl-osc" version))
          (sha256
           (base32 "1x4cavspaxcr604g0hwak1ncy4m9cqq03ji4v56g2h1443n7h48a"))))
-      (build-system asdf-build-system/sbcl)
-      (inputs (list sbcl-ieee-floats))
-      (synopsis "Implementation of the Open Sound Control protocol")
-      (description
-       "This is a lisp implementation of the Open Sound Control protocol (or more accurately “data transport specification” or “encoding”). The code should be close to ANSI standard common lisp and provides self contained code for encoding and decoding of OSC data, messages, and bundles.")
-      (home-page "https://github.com/zzkt/osc/")
-      (license (list license:gpl3+)))))
+     (build-system asdf-build-system/sbcl)
+     (native-inputs (list sbcl-fiveam))
+     (inputs (list sbcl-ieee-floats))
+     (synopsis "Implementation of the Open Sound Control protocol")
+     (description "This is a lisp implementation of the Open Sound Control protocol (or more accurately “data transport specification” or “encoding”).  The code should be close to ANSI standard common lisp and provides self contained code for encoding and decoding of OSC data, messages, and bundles.")
+     (home-page "https://github.com/zzkt/osc/")
+     (license license:gpl3+))))
 
 (define-public cl-osc
   (sbcl-package->cl-source-package sbcl-osc))
