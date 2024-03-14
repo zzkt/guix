a1f634322ecaf3ecbd933c9da15ee879c1f6a922
nik gaffney <nik@fo.am>
2024-03-14 09:10:04 +0100

    Put in earplugs

--- channel/zzkt/packages/mattermost.scm
+++ channel/zzkt/packages/mattermost.scm
@@ -19,11 +19,69 @@
 (define-module (zzkt packages mattermost)
   #:use-module (guix)
   #:use-module (guix build-system copy)
+  #:use-module (guix build-system node)
+  #:use-module (guix git-download)
   #:use-module ((guix licenses)
                 #:prefix license:))
 
+;; binary
 ;; https://releases.mattermost.com/desktop/5.6.0/mattermost-desktop-5.6.0-linux-x64.tar.gz
 
+;; source
+;; https://github.com/mattermost/desktop
+
+;; - create_desktop_file.sh
+;; - mattermost-desktop
+
+(define-public mattermost-desktop-binary
+  (package
+    (name "mattermost-desktop-binary")
+    (version "5.6.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri "https://releases.mattermost.com/desktop/5.6.0/mattermost-desktop-5.6.0-linux-x64.tar.gz")
+       (sha256
+        (base32 "1x0pigcl1a6clrv8zki4m9qnjc15fzz0c0h2y19f8pvwgp67yh99"))))
+    (build-system copy-build-system)
+    (arguments
+     ;; via readme.md
+     ;; /opt/mattermost-desktop/create_desktop_file.sh
+
+     ;; #!/bin/sh
+     ;; set -e
+     ;; WORKING_DIR=`pwd`
+     ;; THIS_PATH=`readlink -f $0`
+     ;; cd `dirname ${THIS_PATH}`
+     ;; FULL_PATH=`pwd`
+     ;; cd ${WORKING_DIR}
+     ;; cat <<EOS > Mattermost.desktop
+     ;; [Desktop Entry]
+     ;; Name=Mattermost
+     ;; Comment=Mattermost Desktop application for Linux
+     ;; Exec="${FULL_PATH}/mattermost-desktop" %U
+     ;; Terminal=false
+     ;; Type=Application
+     ;; Icon=${FULL_PATH}/app_icon.png
+     ;; Categories=Network;InstantMessaging;
+     ;; EOS
+     ;; chmod +x Mattermost.desktop
+
+     ;; mv Mattermost.desktop ~/.local/share/applications/
+     ;; sudo ln -s /opt/mattermost-desktop/mattermost-desktop /usr/local/bin/
+
+     (list #:install-plan
+       #~'(("mattermost-desktop" "bin/mattermost-desktop")
+           ("resources" "resources"))))
+
+    (home-page "https://mattermost.com/")
+    (synopsis "Collaboration platform for technical and operational teams. (binary release)")
+    (description
+     "Work together effectively with real-time communication, file and code snippet sharing, in-line code syntax highlighting, and workflow automation purpose-built for technical teams.")
+    (license license:asl2.0)))
+
+
+;; source
 ;; https://github.com/mattermost/desktop
 
 (define-public mattermost-desktop
