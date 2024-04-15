8034116f2bb1699fbb77fab834ad21621e9234a5
nik gaffney <nik@fo.am>
2024-04-15 11:00:23 +0200

    Put in earplugs

--- channel/zzkt/services/fwupd.scm
+++ channel/zzkt/services/fwupd.scm
@@ -18,53 +18,53 @@
 
 ;; via https://issues.guix.gnu.org/60699
 
-(define-module (gnu services firmware)
-  #:use-module (guix diagnostics)
-  #:use-module (guix gexp)
-  #:use-module (guix records)
-  #:use-module (guix modules)
-  #:use-module (guix i18n)
-  #:use-module (guix ui)
-  #:use-module (gnu services)
-  #:use-module (gnu services base)
-  #:use-module (gnu services shepherd)
-  #:use-module (gnu packages firmware)
-  #:use-module (srfi srfi-1)
-  #:use-module (srfi srfi-26)
-  #:use-module (srfi srfi-34)
-  #:use-module (srfi srfi-35)
-  #:use-module (ice-9 match)
-  #:export (fwupd-configuration fwupd-service-type))
+;; (define-module (gnu services firmware)
+;;   #:use-module (guix diagnostics)
+;;   #:use-module (guix gexp)
+;;   #:use-module (guix records)
+;;   #:use-module (guix modules)
+;;   #:use-module (guix i18n)
+;;   #:use-module (guix ui)
+;;   #:use-module (gnu services)
+;;   #:use-module (gnu services base)
+;;   #:use-module (gnu services shepherd)
+;;   #:use-module (gnu packages firmware)
+;;   #:use-module (srfi srfi-1)
+;;   #:use-module (srfi srfi-26)
+;;   #:use-module (srfi srfi-34)
+;;   #:use-module (srfi srfi-35)
+;;   #:use-module (ice-9 match)
+;;   #:export (fwupd-configuration fwupd-service-type))
 
-;;; Fwupd daemon.
+;; ;;; Fwupd daemon.
 
-(define-record-type* <fwupd-configuration> fwupd-configuration
-  make-fwupd-configuration fwupd-configuration?
-  (fwupd fwupd-configuration-fwupd
-         (default fwupd)))
+;; (define-record-type* <fwupd-configuration> fwupd-configuration
+;;   make-fwupd-configuration fwupd-configuration?
+;;   (fwupd fwupd-configuration-fwupd
+;;          (default fwupd)))
 
-(define fwupd-activation-service
-  #~(begin
-      (use-modules (guix build utils))
-      (mkdir-p "/var/lib/fwupd")))
+;; (define fwupd-activation-service
+;;   #~(begin
+;;       (use-modules (guix build utils))
+;;       (mkdir-p "/var/lib/fwupd")))
 
-(define (fwupd-shepherd-service config)
-  (shepherd-service (documentation "Run the fwupd daemon.")
-                    (provision '(fwupd))
-                    (requirement '(dbus-system udev))
-                    (start #~(make-forkexec-constructor
-                              (list (string-append #$fwupd "/libexec/fwupd/fwupd"))))
-                    (stop #~(make-kill-destructor))))
+;; (define (fwupd-shepherd-service config)
+;;   (shepherd-service (documentation "Run the fwupd daemon.")
+;;                     (provision '(fwupd))
+;;                     (requirement '(dbus-system udev))
+;;                     (start #~(make-forkexec-constructor
+;;                               (list (string-append #$fwupd "/libexec/fwupd/fwupd"))))
+;;                     (stop #~(make-kill-destructor))))
 
-(define fwupd-service-type
-  (service-type (name 'fwupd)
-                (default-value (fwupd-configuration))
-                (extensions
-                 (list (service-extension activation-service-type
-                                          (const
-                                           fwupd-activation-service))
-                       (service-extension
-                        shepherd-root-service-type
-                        (compose list fwupd-shepherd-service))))
-                (description
-                 "Run @command{fwupd}, the Firmware update daemon.")))
+;; (define fwupd-service-type
+;;   (service-type (name 'fwupd)
+;;                 (default-value (fwupd-configuration))
+;;                 (extensions
+;;                  (list (service-extension activation-service-type
+;;                                           (const
+;;                                            fwupd-activation-service))
+;;                        (service-extension
+;;                         shepherd-root-service-type
+;;                         (compose list fwupd-shepherd-service))))
+;;                 (description
+;;                  "Run @command{fwupd}, the Firmware update daemon.")))
