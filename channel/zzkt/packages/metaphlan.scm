09d8c4a554d2f151c9b721e10a7e292a73cf4301
nik gaffney <nik@fo.am>
2025-06-04 13:31:21 +0200

    Put in earplugs

--- channel/zzkt/packages/metaphlan.scm
+++ channel/zzkt/packages/metaphlan.scm
@@ -1,48 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2014-2025 Ricardo Wurmus <rekado@elephly.net>
-;;; Copyright © 2015, 2016, 2017, 2018 Ben Woodcroft <donttrustben@gmail.com>
-;;; Copyright © 2015, 2016, 2018, 2019, 2020 Pjotr Prins <pjotr.guix@thebird.nl>
-;;; Copyright © 2015 Andreas Enge <andreas@enge.fr>
-;;; Copyright © 2016, 2020, 2021 Roel Janssen <roel@gnu.org>
-;;; Copyright © 2016-2024 Efraim Flashner <efraim@flashner.co.il>
-;;; Copyright © 2016, 2020, 2022 Marius Bakke <marius@gnu.org>
-;;; Copyright © 2016, 2018 Raoul Bonnal <ilpuccio.febo@gmail.com>
-;;; Copyright © 2017, 2018 Tobias Geerinckx-Rice <me@tobias.gr>
-;;; Copyright © 2017, 2021, 2022, 2024, 2025 Arun Isaac <arunisaac@systemreboot.net>
-;;; Copyright © 2018 Joshua Sierles, Nextjournal <joshua@nextjournal.com>
-;;; Copyright © 2018 Gábor Boskovits <boskovits@gmail.com>
-;;; Copyright © 2018-2024 Mădălin Ionel Patrașcu <madalinionel.patrascu@mdc-berlin.de>
-;;; Copyright © 2019, 2020, 2021, 2023 Maxim Cournoyer <maxim.cournoyer@gmail.com>
-;;; Copyright © 2019 Brian Leung <bkleung89@gmail.com>
-;;; Copyright © 2019 Brett Gilio <brettg@gnu.org>
-;;; Copyright © 2020 Björn Höfling <bjoern.hoefling@bjoernhoefling.de>
-;;; Copyright © 2020 Jakub Kądziołka <kuba@kadziolka.net>
-;;; Copyright © 2020 Pierre Langlois <pierre.langlois@gmx.com>
-;;; Copyright © 2020 Bonface Munyoki Kilyungi <bonfacemunyoki@gmail.com>
-;;; Copyright © 2021 Tim Howes <timhowes@lavabit.com>
-;;; Copyright © 2021 Hong Li <hli@mdc-berlin.de>
-;;; Copyright © 2021, 2022, 2023 Simon Tournier <zimon.toutoune@gmail.com>
-;;; Copyright © 2021 Felix Gruber <felgru@posteo.net>
-;;; Copyright © 2022-2025 Navid Afkhami <navid.afkhami@mdc-berlin.de>
-;;; Copyright © 2022 Antero Mejr <antero@mailbox.org>
-;;; Copyright © 2024 Alexis Simon <alexis.simon@runbox.com>
-;;; Copyright © 2024 Spencer King <spencer.king@geneoscopy.com>
-;;; Copyright © 2025 nomike Postmann <nomike@nomike.com>
-;;;
-;;; This file is part of GNU Guix.
-;;;
-;;; GNU Guix is free software; you can redistribute it and/or modify it
-;;; under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 3 of the License, or (at
-;;; your option) any later version.
-;;;
-;;; GNU Guix is distributed in the hope that it will be useful, but
-;;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+;; based on /gnu/packages/bioinformatics.scm
 
 (define-module (zzkt packages metaphlan)
   #:use-module ((guix licenses) #:prefix license:)
@@ -51,22 +9,9 @@
   #:use-module (guix utils)
   #:use-module (guix download)
   #:use-module (guix git-download)
-  #:use-module (guix hg-download)
-  #:use-module (guix build-system ant)
   #:use-module (guix build-system gnu)
-  #:use-module (guix build-system cargo)
-  #:use-module (guix build-system cmake)
-  #:use-module (guix build-system go)
-  #:use-module (guix build-system haskell)
-  #:use-module (guix build-system meson)
-  #:use-module (guix build-system perl)
   #:use-module (guix build-system pyproject)
   #:use-module (guix build-system python)
-  #:use-module (guix build-system qt)
-  #:use-module (guix build-system r)
-  #:use-module (guix build-system ruby)
-  #:use-module (guix build-system trivial)
-  #:use-module (guix deprecation)
   #:use-module (gnu packages)
   #:use-module (gnu packages admin)
   #:use-module (gnu packages assembly)
@@ -76,6 +21,7 @@
   #:use-module (gnu packages bash)
   #:use-module (gnu packages bison)
   #:use-module (gnu packages bioconductor)
+  #:use-module (gnu packages bioinformatics)
   #:use-module (gnu packages boost)
   #:use-module (gnu packages check)
   #:use-module (gnu packages code)
@@ -83,10 +29,6 @@
   #:use-module (gnu packages compression)
   #:use-module (gnu packages cpp)
   #:use-module (gnu packages cpio)
-  #:use-module (gnu packages cran)
-  #:use-module (gnu packages crates-compression)
-  #:use-module (gnu packages crates-io)
-  #:use-module (gnu packages crates-web)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages docbook)
   #:use-module (gnu packages documentation)
@@ -98,10 +40,6 @@
   #:use-module (gnu packages gawk)
   #:use-module (gnu packages gcc)
   #:use-module (gnu packages gd)
-  #:use-module (gnu packages golang-build)
-  #:use-module (gnu packages golang-check)
-  #:use-module (gnu packages golang-compression)
-  #:use-module (gnu packages golang-xyz)
   #:use-module (gnu packages glib)
   #:use-module (gnu packages gnupg)
   #:use-module (gnu packages graph)
@@ -111,16 +49,9 @@
   #:use-module (gnu packages gtk)
   #:use-module (gnu packages guile)
   #:use-module (gnu packages guile-xyz)
-  #:use-module (gnu packages haskell-check)
-  #:use-module (gnu packages haskell-web)
-  #:use-module (gnu packages haskell-xyz)
   #:use-module (gnu packages image)
   #:use-module (gnu packages image-processing)
   #:use-module (gnu packages imagemagick)
-  #:use-module (gnu packages java)
-  #:use-module (gnu packages java-compression)
-  #:use-module (gnu packages java-xml)
-  #:use-module (gnu packages jemalloc)
   #:use-module (gnu packages jupyter)
   #:use-module (gnu packages libffi)
   #:use-module (gnu packages linux)
@@ -184,7 +115,7 @@
   #:use-module (srfi srfi-26)
   #:use-module (ice-9 match))
 
-;; based on /gnu/packages/bioinformatics.scm
+
 
 (define-public metaphlan
   (package
@@ -218,4 +149,3 @@ shotgun sequencing data (i.e.  not 16S) with species-level.  With the newly
 added @code{StrainPhlAn} module, it is now possible to perform accurate
 strain-level microbial profiling.")
     (license #f)))
-
