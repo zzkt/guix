0766f261512529a21f85f5595b26f70f841b0c17
nik gaffney <nik@fo.am>
2025-06-05 11:26:40 +0200

    Put in earplugs

--- channel/zzkt/packages/metaphlan.scm
+++ channel/zzkt/packages/metaphlan.scm
@@ -50,10 +50,13 @@
     (native-inputs (list python-setuptools python-wheel))
     (home-page "http://github.com/biobakery/MetaPhlAn/")
     (arguments
-     (list
-      ;; set database directory to a writable location
-      (wrap-program (string-append %output "/bin/metaphlan")
-                  '("METAPHLAN_DB_DIR" = "$HOME/metaphlan_databases/"))))
+      #:modules ((guix build utils))
+      #:phases
+       (modify-phases %standard-phases
+         (add-after 'install 'wrap-program
+                    ;; set database directory to a writable location
+                    (wrap-program (string-append %output "/bin/metaphlan")
+                                  '("METAPHLAN_DB_DIR" = "$HOME/metaphlan_databases/")))))
     (synopsis
      "Profiling of microbial communities from Metagenomic Shotgun Sequencing data.")
     (description
