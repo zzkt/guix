d575eca5328f3b6945f48f2d3e441259613762bb
nik gaffney <nik@fo.am>
2025-06-05 13:36:46 +0200

    Put in earplugs

--- channel/zzkt/packages/metaphlan.scm
+++ channel/zzkt/packages/metaphlan.scm
@@ -58,7 +58,13 @@
               (lambda* _
              ;; set database directory to a writable location
                 (wrap-program (string-append #$output "/bin/metaphlan")
-                             '("METAPHLAN_DB_DIR" = ("$HOME/metaphlan_databases/"))))))))
+                             '("METAPHLAN_DB_DIR" = ("/var/lib/metaphlan/metaphlan_databases/"))))))))
+    ;; search paths for shared databases
+     (search-paths
+      (list (search-path-specification
+          (variable "METAPHLAN_DB_DIR")
+          (separator #f)
+          (files (list "/var/lib/metaphlan/metaphlan_databases/")))))
     (synopsis
      "Profiling of microbial communities from Metagenomic Shotgun Sequencing data.")
     (description
