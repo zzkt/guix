2cdb5ba65439113d2fe0d74bf485fb50d4a4ac41
nik gaffney <nik@fo.am>
2025-09-16 13:53:02 +0200

    Put in Earplugs

--- channel/zzkt/packages/ghostty.scm
+++ channel/zzkt/packages/ghostty.scm
@@ -29,8 +29,12 @@
   #:use-module (guix git-download)
   #:use-module (guix packages)
   #:use-module (guix utils)
-  #:use-module (saayix packages gtk)
+  ;; #:use-module (saayix packages gtk) ;; ->  gtk4-layer-shell
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages)
+  #:use-module (guix build-system meson)
   #:export (ghostty))
+
 
 (define ghostty
   (package
@@ -424,4 +428,41 @@ being fast, feature-rich, and native. While there are many excellent terminal
 emulators available, they all force you to choose between speed, features, or
 native UIs. Ghostty provides all three.")
     (home-page "https://ghostty.org")
+    (license license:expat)))
+
+
+(define gtk4-layer-shell
+  (package
+    (name "gtk4-layer-shell")
+    (version "1.1.1")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://github.com/wmww/gtk4-layer-shell")
+               (commit (string-append "v" version))))
+        (file-name (git-file-name name version))
+        (sha256
+          (base32 "00z6y4z1m7yji1hff58m97k2f5zzmij67nh70dfz18vw5qmm0c75"))))
+    (build-system meson-build-system)
+    (arguments
+      (list #:tests? #f
+            #:configure-flags
+            #~(list "-Ddocs=true"
+                    "-Dexamples=true")))
+    (native-inputs
+      (list pkg-config
+            gobject-introspection
+            gtk-doc
+            vala))
+    (inputs
+      (list gtk
+            wayland
+            wayland-protocols))
+    (synopsis "A library to create panels and other desktop components for
+Wayland using the Layer Shell protocol and GTK4")
+    (description "A library for using the Layer Shell and Session Lock Wayland
+protocols with GTK4. This Library is compatible with C, C++ and any language
+that supports GObject introspection files (Python, Vala, etc).")
+    (home-page "https://github.com/wmww/gtk4-layer-shell")
     (license license:expat)))
