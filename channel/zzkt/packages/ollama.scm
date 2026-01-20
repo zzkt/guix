d9b5101198503b2d7009daaf3d9ef9ab03c7ddd5
nik gaffney <nik@fo.am>
2026-01-20 17:46:11 +0100

    Put in earplugs

--- channel/zzkt/packages/ollama.scm
+++ channel/zzkt/packages/ollama.scm
@@ -12,9 +12,15 @@
 
 ;; https://github.com/ollama/ollama/releases/download/v0.14.3-rc2/ollama-linux-amd64-rocm.tar.zst
 
-(define-public ollama
+;; requires (?)
+;; - rocminfo
+;; - rocm-device-libs
+;; - rocm-opencl-runtime
+;; - amd-smi
+
+(define-public ollama-rocm
   (package
-    (name "ollama")
+    (name "ollama-rocm")
     (version "0.14.3-rc2")
     (source
      (origin
@@ -26,29 +32,34 @@
         (base32 "0d1h76q40f03npkbg7b0i6gqrwyb2f4mg5mahl7mmbh1j8q7dfy7"))))
     (build-system binary-build-system)
     (supported-systems (list "x86_64-linux"))
-    (arguments
-     `(#:strip-binaries? #f
-       #:patchelf-plan `(("ollama" ("glibc" "gcc")))
-       #:install-plan `(("ollama" "bin/ollama"))
-       #:phases (modify-phases %standard-phases
-                  (add-after 'binary-unpack 'chmod-to-allow-patchelf
-                    (lambda _
-                      (chmod "ollama" #o755))))))
+    ;; (arguments
+    ;;  `(#:strip-binaries? #f
+    ;;    #:patchelf-plan `(("ollama" ("glibc" "gcc")))
+    ;;    #:install-plan `(("ollama" "bin/ollama"))
+    ;;    #:phases (modify-phases %standard-phases
+    ;;               (add-after 'binary-unpack 'chmod-to-allow-patchelf
+    ;;                 (lambda _
+    ;;                   (chmod "ollama" #o755))))))
     (inputs (list (list gcc "lib") glibc))
     (home-page "https://ollama.com")
     (synopsis "Get up and running with large language models")
     (description
      "Get up and running with large language models.
-Run Llama 2, Code Llama, and other models. Customize and create your own.
-Using binary -> ollama-linux-amd64-rocm")
+Run Llama 2, Code Llama, and other models. Customize and create your own.")
     (license license:expat)))
 
+;; v0.14.2 (stable)
 ;; https://github.com/ollama/ollama/releases/download/v0.14.2/ollama-linux-amd64.tar.zst
+;; 07r7856b37sj1775vjgjx3xsi4wczb8hdf4l5blkq595lzwfjnm6
 
-(define-public ollama-linux-amd64
+;; v0.14.3 (pre-release)
+;; https://github.com/ollama/ollama/releases/download/v0.14.3-rc2/ollama-linux-amd64.tar.zst
+;; 1xbd4cjsvv6wq16368rww9pr50zwj9fpcm72dhdr1j0rj9c3z3p3
+
+(define-public ollama
   (package
-    (name "ollama-linux-amd64")
-    (version "0.14.2")
+    (name "ollama")
+    (version "0.14.3-rc2")
     (source
      (origin
        (method url-fetch)
@@ -56,7 +67,7 @@ Using binary -> ollama-linux-amd64-rocm")
              "https://github.com/ollama/ollama/releases/download/v" version
              "/ollama-linux-amd64.tar.zst"))
        (sha256
-        (base32 "07r7856b37sj1775vjgjx3xsi4wczb8hdf4l5blkq595lzwfjnm6"))))
+        (base32 "1xbd4cjsvv6wq16368rww9pr50zwj9fpcm72dhdr1j0rj9c3z3p3"))))
     (build-system binary-build-system)
     (supported-systems (list "x86_64-linux"))
     (arguments
